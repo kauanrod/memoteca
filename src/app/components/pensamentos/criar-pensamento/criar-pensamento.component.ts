a6d8d65b7a146bb5a8ba292f6824ff48a45020b5
Kauan Rodrigues <kauanrod@hotmail.com>
2023-08-23 21:19:25 -0300

    Data Binding

--- src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/components/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-criar-pensamento',
   templateUrl: './criar-pensamento.component.html',
-  styleUrls: ['./criar-pensamento.component.css']
+  styleUrls: ['./criar-pensamento.component.css'],
 })
 export class CriarPensamentoComponent {
+  pensamento = {
+    id: '1',
+    conteudo: 'Aprendendo Angular',
+    autoria: 'Dev',
+    modelo: 'modelo1',
+  };
 
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  criarPensamento(): void {
+    return alert('Novo pensamento criado.');
+  }
+
+  cancelar(): void {
+    return alert('Ação cancelada.');
+  }
 }
